6a8fc5971bf89d4afcab755ff38dd54fe24f2eaa
Yaroslav Yarkevich <yaroslavyarkevich@gmail.com>
2024-01-29 02:00:14 +0200

    solved an exersice

---
+++ codeWars/breakCamelCase.js
@@ -0,0 +1,21 @@
+//My Solution.
+const someString = "camelCasing";
+// const checkCap = (some)=>{
+//     let result ='';
+//     let capLetter =0;
+//     for(let index = 0; index < some.length; index++){
+//         if(some[index].toUpperCase()=== some[index]){
+//             result += `${some.slice(capLetter, index)} `;
+//             capLetter = index;
+//         }
+//     }
+//     return result += `${some.slice(capLetter, some.length)}`;
+    
+// }
+
+const checkCap2 = string=>{
+    let res = '';
+    let letters = string.split("");
+    console.log(letters);
+}
+checkCap2(someString);
