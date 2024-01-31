c195f5ef71067bad56a3e66cca39f2df4757a543
Yaroslav Yarkevich <yaroslavyarkevich@gmail.com>
2024-01-31 13:05:07 +0200

    Solved Bus challenge

---
+++ codeWars/Bus.js
@@ -0,0 +1,11 @@
+//([[10,0],[3,5],[5,8]]),5
+//([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]]),17
+const number = (busStops)=>{
+    let totalNumber = 0;
+    for(let index = 0; index < busStops.length; index++){
+        totalNumber += busStops[index][0];
+        totalNumber -= busStops[index][1];
+    }
+    return totalNumber;
+}
+console.log(number([[3,0],[9,1],[4,8],[12,2],[6,1],[7,8]]));
