6ebede4a95145d9864ae3af514b98c2d64887f02
Yaroslav Yarkevich <yaroslavyarkevich@gmail.com>
2024-02-22 22:34:37 +0200

    didn't solve regex exer

---
+++ codeWars/convertToCamel.js
@@ -0,0 +1,20 @@
+/* 
+Complete the method/function so that it converts dash/underscore 
+delimited words into camel casing. The first word within the output 
+should be capitalized only if the original word was capitalized 
+(known as Upper Camel Case, also often referred to as Pascal case).
+ The next words should be always capitalized.
+
+Examples
+"the-stealth-warrior" gets converted to "theStealthWarrior"
+
+"The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+
+"The_Stealth-Warrior" gets converted to "TheStealthWarrior"
+*/
+function toCamelCase(str){
+    const myRegex = /(?<=[_-])(.)/g ;
+    const newStr = str.replace(myRegex);
+    return newStr;
+}
+console.log(toCamelCase("the-stealth-warrior"));
