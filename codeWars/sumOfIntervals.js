4070130d19eef885f6244251e255315d9d5fa374
Yaroslav Yarkevich <yaroslavyarkevich@gmail.com>
2024-10-06 19:58:38 +0300

    two forloop dont work for solution of that exersice

---
+++ codeWars/sumOfIntervals.js
@@ -0,0 +1,52 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sumOfIntervals = void 0;
+function sumOfIntervals(intervals) {
+    //first i enter a for loop fixate on first interval check if there are intervals that start and end inside 
+    //Current interval or start before current interval and end inside or start inside current interval and end 
+    //outside
+    var checkForOverlaps = function (annArray) {
+        var noOverlap = [];
+        for (var index = 0; index < intervals.length; index++) {
+            var currentInterval = intervals[index];
+            for (var i = 1; i < intervals.length; i++) {
+                if (intervals[i][0] > currentInterval[0] && intervals[i][1] < currentInterval[1]) {
+                    noOverlap.push(currentInterval);
+                }
+                else if (intervals[i][0] < currentInterval[0] && (intervals[i][1] >= currentInterval[0] && intervals[i][1] <= currentInterval[1])) {
+                    noOverlap.push([intervals[i][0], currentInterval[1]]);
+                }
+                else if (intervals[i][0] <= currentInterval[0] && intervals[i][1] >= currentInterval[1]) {
+                    noOverlap.push(intervals[i]);
+                }
+            }
+            noOverlap.push(currentInterval);
+        }
+        return noOverlap;
+    };
+    console.log(checkForOverlaps(intervals));
+}
+exports.sumOfIntervals = sumOfIntervals;
+//Examples
+sumOfIntervals([
+    [1, 2],
+    [6, 10],
+    [11, 15]
+]);
+sumOfIntervals([
+    [1, 4],
+    [7, 10],
+    [3, 5]
+]);
+sumOfIntervals([
+    [1, 5],
+    [10, 20],
+    [1, 6],
+    [16, 19],
+    [5, 11]
+]);
+sumOfIntervals([
+    [0, 20],
+    [-100000000, 10],
+    [30, 40]
+]);
