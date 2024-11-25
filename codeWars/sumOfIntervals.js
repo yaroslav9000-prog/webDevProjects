c6c3c929e4d99175c52498323e56f9351fc1c09e
Yaroslav Yarkevich <yaroslavyarkevich@gmail.com>
2024-11-25 23:59:19 +0200

    solved but crash because of size

--- codeWars/sumOfIntervals.js
+++ codeWars/sumOfIntervals.js
@@ -1,30 +1,56 @@
 "use strict";
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.sumOfIntervals = void 0;
+var checkIfOverlap = function (firstArray, secondArray) {
+    var combinedArray = __spreadArray(__spreadArray([], firstArray, true), secondArray, true);
+    if (combinedArray[0] > combinedArray[2] && combinedArray[1] >= combinedArray[3]) {
+        return [combinedArray[2], combinedArray[1]];
+    }
+    else if (combinedArray[0] <= combinedArray[2] && combinedArray[1] >= combinedArray[3]) {
+        return [combinedArray[0], combinedArray[1]];
+    }
+    else if (combinedArray[0] <= combinedArray[2] && combinedArray[3] > combinedArray[1]) {
+        return [combinedArray[0], combinedArray[3]];
+    }
+    else if (combinedArray[0] > combinedArray[2] && combinedArray[1] < combinedArray[3]) {
+        return [combinedArray[2], combinedArray[3]];
+    }
+    else {
+        return [];
+    }
+};
 function sumOfIntervals(intervals) {
     //first i enter a for loop fixate on first interval check if there are intervals that start and end inside 
     //Current interval or start before current interval and end inside or start inside current interval and end 
     //outside
-    var checkForOverlaps = function (annArray) {
-        var noOverlap = [];
-        for (var index = 0; index < intervals.length; index++) {
-            var currentInterval = intervals[index];
-            for (var i = 1; i < intervals.length; i++) {
-                if (intervals[i][0] > currentInterval[0] && intervals[i][1] < currentInterval[1]) {
-                    noOverlap.push(currentInterval);
-                }
-                else if (intervals[i][0] < currentInterval[0] && (intervals[i][1] >= currentInterval[0] && intervals[i][1] <= currentInterval[1])) {
-                    noOverlap.push([intervals[i][0], currentInterval[1]]);
-                }
-                else if (intervals[i][0] <= currentInterval[0] && intervals[i][1] >= currentInterval[1]) {
-                    noOverlap.push(intervals[i]);
-                }
+    /*
+    [1, 5],
+   [10, 20],
+   [1, 6],
+   [16, 19],
+   [5, 11]
+    */
+    var newArray = [];
+    for (var index = 0; index < intervals.length; index++) {
+        var firstArray = intervals[index];
+        for (var i = 1; i < intervals.length; i++) {
+            var secondArray = intervals[i];
+            var result = checkIfOverlap(firstArray, secondArray);
+            if (result.length > 0) {
+                newArray.push(result);
             }
-            noOverlap.push(currentInterval);
         }
-        return noOverlap;
-    };
-    console.log(checkForOverlaps(intervals));
+    }
+    return newArray;
 }
 exports.sumOfIntervals = sumOfIntervals;
 //Examples
--- codeWars/sumOfIntervals.ts
+++ codeWars/sumOfIntervals.ts
@@ -1,52 +1,91 @@
-export function sumOfIntervals(intervals: [number, number][]):void {
+// const checkIfOverlap=(firstArray: number[], secondArray: number[])=>{
+//     if(firstArray[0] > secondArray[0] && (firstArray[1] > secondArray[1] && firstArray[0] < secondArray[1])){
+//         return [secondArray[0], firstArray[1]];
+//     } 
+//     if((secondArray[0] > firstArray[0] && secondArray[0] < firstArray[1]) && (secondArray[1] > firstArray[0] && secondArray[1] < firstArray[1])){
+//         return firstArray;
+//     }
+//     if((secondArray[0] >= firstArray[0] && secondArray[0] < firstArray[1]) && secondArray[1] > firstArray[1]){
+//         return [firstArray[0], secondArray[1]];
+//     }
+//     if((firstArray[0] >= secondArray[0] && firstArray[0] <= secondArray[1]) && (firstArray[1] <= secondArray[1] && firstArray[1] >= secondArray[0])){
+//         return [secondArray[0], secondArray[1]];
+//     }else{
+//         return [];
+//     }
+// }
+
+function sumOfIntervals(intervals: [number, number][]) {
     //first i enter a for loop fixate on first interval check if there are intervals that start and end inside 
     //Current interval or start before current interval and end inside or start inside current interval and end 
     //outside
-   const checkForOverlaps =(annArray : [number, number][]): [number, number][]=>{
-
-   
-   const noOverlap: [number, number][] = []
+    /*
+    [1, 5],
+   [10, 20],
+   [1, 6],
+   [16, 19],
+   [5, 11]
+    */
+//    const newIntervals = [...intervals];
+//    const newArray: any = [];
+//     for(let index = 0; index <= newIntervals.length - 2; index++){
+//         let firstArray = newIntervals[index];
+//         for(let i= index + 1; i < newIntervals.length; i++){
+//             let secondArray = newIntervals[i];
+//             console.log(firstArray, secondArray, "\n---------------------");
+//             const result = checkIfOverlap(firstArray, secondArray);
+//             if(result.length > 0){
+//                 console.log('the result is: \n', result, '\n--------------');
+//                 newArray.push(result);
+//             }
+//         }
+//     }
+//     return newArray;
+    const newArray:number[] = [];
     for(let index = 0; index < intervals.length; index++){
-        const currentInterval = intervals[index];
-        for(let i = 1; i < intervals.length; i++){
-            if(intervals[i][0] > currentInterval[0] && intervals[i][1] < currentInterval[1]){
-                noOverlap.push(currentInterval)
-            }else if(intervals[i][0] < currentInterval[0] && (intervals[i][1] >= currentInterval[0] && intervals[i][1]<= currentInterval[1])){
-                noOverlap.push([intervals[i][0], currentInterval[1]]);
-            }else if(intervals[i][0] <= currentInterval[0] && intervals[i][1] >= currentInterval[1]){
-                noOverlap.push(intervals[i]);
-            }
+        for(let i = intervals[index][0]; i < intervals[index][1]; i++){
+            newArray.push(i);
         }
-        noOverlap.push(currentInterval);
     }
-    return noOverlap;
+    const someObj:{[index: string]:any} = {};
+    for(let index = 0; index < newArray.length; index++){
+        if(someObj[`${newArray[index]}`] ==undefined){
+            someObj[`${newArray[index]}`] = 1;
+        }else{
+            someObj[`${newArray[index]}`]++;
+        }
+    }
+    let length = 0;
+    for(let item in someObj){
+        length++
+    }
+   return length;
 }
-    console.log(checkForOverlaps(intervals));
-  }
-
+// console.log(checkIfOverlap([1,2], [1,7]));
+// console.log(checkIfOverlap([1,2], [6,10]))
   //Examples
-  sumOfIntervals( [
-   [1, 2],
-   [6, 10],
-   [11, 15]
-] ) 
+//   console.log(sumOfIntervals( [
+//    [1, 2],
+//    [6, 10],
+//    [11, 15]
+// ] ) )
 
-sumOfIntervals( [
-   [1, 4],
-   [7, 10],
-   [3, 5]
-] )
+// console.log(sumOfIntervals( [
+//    [1, 4],
+//    [7, 10],
+//    [3, 5]
+// ] ))
 
-sumOfIntervals( [
+console.log(sumOfIntervals( [
    [1, 5],
    [10, 20],
    [1, 6],
    [16, 19],
    [5, 11]
-] )
+] ))
 
-sumOfIntervals( [
-   [0, 20],
-   [-100000000, 10],
-   [30, 40]
-] )
+// console.log(sumOfIntervals( [
+//    [0, 20],
+//    [-100000000, 10],
+//    [30, 40]
+// ] ))
